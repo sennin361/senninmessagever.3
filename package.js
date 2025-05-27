bd5de457aa0a3cec1833904d4ca4401e57360c1c
sennin361 <pokeopoke9@gmail.com>
2025-05-27 20:13:46 +0900

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "sennin-chat",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "engines": {
+    "node": "18.x"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.7.1",
+    "multer": "^1.4.5"
+  }
+}
